a2e231f0f7bb664c767fab9e08d7075f69537b24
Sarah Norman <snorm045@gmail.com>
2025-03-12 19:40:45 -0400

    commit from the seventh ring

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, FlatList, TextInput, Button, View, Text} from 'react-native';
+import TaskItem from './components/ManageTasks'; 
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    { key: "1", title: "Clean Room", completed: false },
+    { key: "2", title: "Do Laundry", completed: false },
+    { key: "3", title: "Walk Dog", completed: false }
+  ]);
+
+  const [inputText, setInputText] = useState(""); 
+
+  const addTask = () => {
+    if (inputText.trim()) {
+      setTasks([
+        ...tasks,
+        {
+          key: (tasks.length + 1).toString(),
+          title: inputText,
+          completed: false
+        }
+      ]);
+      setInputText("");  
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.taskList}>
+      <Text style={styles.heading}>To Do List</Text>
+        <View>
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => (
+              <TaskItem 
+                task={item} 
+              />
+            )}
+          />
+        </View>
+
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter a new task"
+            value={inputText}
+            onChangeText={setInputText}
+          />
+          <Button title="Add Task" onPress={addTask} />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f7f7f5",
+    justifyContent: "flex-start",
+    
   },
+  taskList: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginLeft: 20,
+    marginBottom: 10
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingLeft: 10,
+    flex: 1,
+    marginRight: 10,
+  },
+
 });
---
+++ components/ManageTasks.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { CheckBox } from '@rneui/themed'; 
+
+export default function TaskItem({ task }) {
+
+  const [isChecked, setIsChecked] = useState(task.completed);
+
+  const handleCheckBoxPress = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <CheckBox
+      checked={isChecked}  
+      onPress={handleCheckBoxPress}  
+      title={task.title}
+      iconType="material-community"
+      checkedIcon="checkbox-outline"
+      uncheckedIcon="checkbox-blank-outline"
+      checkedColor="#387838"
+      uncheckedColor="#323332"
+      containerStyle={{ backgroundColor: "#cfdffa", borderRadius: 4, borderWidth: 0 }}
+      textStyle={isChecked ? { textDecorationLine: "line-through",  color: "#323332"} : {}}
+    />
+  );
+}
